9ae9d0f26ce3adc8bdf907ef52432de1b5bf6075
Philipe Almeida <philipe.almeida@diconium.com>
2024-04-11 11:58:51 +0100

    chore: normalizing main.tsx import

--- FRONT/src/main.tsx
+++ FRONT/src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from '../src/pages/home/Home';
+import Home from './pages/home/Home';
 import '../src/styles/main.css';
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
